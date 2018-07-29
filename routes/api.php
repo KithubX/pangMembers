900f4b151570a780f1821a8b768aa3e12932d3a6
Franz Noel Tanglao <franz@tanglao.org>
2018-07-29 17:58:09 +0000

    feat: Add Members database, resource, and model

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
+
 use Illuminate\Http\Request;
+use App\Members;
+use App\Http\Resources\MemberCollection;
+
+
 Route::group([
     'prefix' => 'auth'
 ], function () {
@@ -11,5 +16,13 @@ Route::group([
     ], function() {
         Route::get('logout', 'AuthController@logout');
         Route::get('user', 'AuthController@user');
+    });
+});
+
+Route::group([ 'prefix' => 'v1'], function() {
+    Route::group(['middleware' => 'auth:api'], function() {
+        Route::get('members', function() {
+            return new MemberCollection(Members::paginate());
+        });
     });
 });
